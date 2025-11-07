8a766a2aaf51def36d624b56286d53d010de66fa
HP <HP@User>
2025-11-07 21:08:51 +0530

    commit

--- StudentServices/src/main/java/com/tnsif/StudentServices/config/WebConfig.java
+++ StudentServices/src/main/java/com/tnsif/StudentServices/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.tnsif.StudentServices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
 public class WebConfig {
 
     @Bean
@@ -14,7 +16,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
             }
         };
     }
